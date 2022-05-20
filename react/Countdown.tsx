0f2c99ab00cbd3d60414cdeceb6126638ea4af97
Lorenzo Durante <lolodurante2003@gmail.com>
2022-05-20 16:28:20 -0300

    Timer

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,38 @@
 import React from 'react'
+import {useState} from 'react'
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
+interface CountdownProps {
+  targetDate : string
+}
 
-interface CountdownProps {}
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div />
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate}) => {
+  const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+  const [timeRemaining, setTimeRemaining] = useState<TimeSplit>({
+    hours : '00',
+    minutes : '00',
+    seconds : '00'
+  })
+  tick(targetDate, setTimeRemaining)
+  return (
+    <div>
+      <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>xx
+    </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetData : {
+      title:'Final title',
+      description : 'This is the description',
+      type : 'string',
+      default : 'null',
+    },
+  },
 }
 
 export default Countdown
